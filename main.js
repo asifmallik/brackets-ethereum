63f2ed2f3189a68fb38b1e840e242939550474d4
Asif Mallik <asif.mallik1@gmail.com>
2017-09-09 08:11:00 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,88 @@
+define(function (require, exports, module) {
+
+	var LanguageManager = brackets.getModule("language/LanguageManager");
+	var CodeMirror = brackets.getModule("thirdparty/CodeMirror/lib/codemirror");
+
+
+	var types = ['address', 'bool', 'string', 'var', 'int', 'int8', 'int16', 'int24', 'int32', 'int40', 'int48', 'int56', 'int64', 'int72', 'int80', 'int88', 'int96', 'int104', 'int112', 'int120', 'int128', 'int136', 'int144', 'int152', 'int160', 'int168', 'int176', 'int184', 'int192', 'int200', 'int208', 'int216', 'int224', 'int232', 'int240', 'int248', 'int256', 'uint', 'uint8', 'uint16', 'uint24', 'uint32', 'uint40', 'uint48', 'uint56', 'uint64', 'uint72', 'uint80', 'uint88', 'uint96', 'uint104', 'uint112', 'uint120', 'uint128', 'uint136', 'uint144', 'uint152', 'uint160', 'uint168', 'uint176', 'uint184', 'uint192', 'uint200', 'uint208', 'uint216', 'uint224', 'uint232', 'uint240', 'uint248', 'uint256', 'byte', 'bytes', 'bytes1', 'bytes2', 'bytes3', 'bytes4', 'bytes5', 'bytes6', 'bytes7', 'bytes8', 'bytes9', 'bytes10', 'bytes11', 'bytes12', 'bytes13', 'bytes14', 'bytes15', 'bytes16', 'bytes17', 'bytes18', 'bytes19', 'bytes20', 'bytes21', 'bytes22', 'bytes23', 'bytes24', 'bytes25', 'bytes26', 'bytes27', 'bytes28', 'bytes29', 'bytes30', 'bytes31', 'bytes32'];
+
+	var typesMatchString = "(";
+
+	for (var i = 0; i < types.length; i++) {
+		typesMatchString += types[i];
+		if (types.length != i + 1) typesMatchString += "|";
+	}
+
+	typesMatchString += ")\\b";
+	var typesMatch = new RegExp(typesMatchString);
+
+	CodeMirror.defineMode("solidity", function (config, parserConfig) {
+		return {
+			startState: function () {
+				return {
+					blockDeclaration: false
+				};
+			},
+			token: function (stream, state) {
+
+
+				if (stream.match(/"(?:[^\\]|\\.)*?(?:"|$)/) || stream.match(/'(?:[^\\]|\\.)*?(?:'|$)/)) {
+					return "string";
+				}
+
+				if (stream.peek() == ".") {
+					state.property = true;
+					stream.next();
+					return null;
+				}
+
+				if (state.property) {
+					state.property = false;
+					if (stream.eatWhile(/[a-zA-Z$_][\w$]*/)) {
+						return "property";
+					} else {
+						stream.next();
+					}
+				}
+
+				if (stream.match(/(memory|storage|pure|view|import|as|from|\*|pragma|return|if|for|while|else|this|returns|external|internal|public|mapping|payable|constant|require)\b/) || stream.match(typesMatch)) {
+					return "keyword";
+				}
+
+				if (stream.match(/(contract|modifier|function|library|struct)\b/)) {
+					state.blockDeclaration = true;
+					return "keyword";
+				}
+
+				if (stream.match(/true|false/)) {
+					return "atom";
+				}
+
+				if (state.blockDeclaration) {
+					if (stream.match(/.+?(?=({|\())/)) {
+						state.blockDeclaration = false;
+						return "property";
+					}
+				}
+
+				if (stream.match(/0x[a-f\d]+|[-+]?(?:\.\d+|\d+\.?\d*)(?:e[-+]?\d+)?/i)) {
+					return "number";
+				}
+
+				if (stream.eatWhile(/[a-zA-Z_$][\w$]*/)) {
+					return "variable";
+				} else {
+					stream.next();
+					return null;
+				}
+			}
+		};
+
+	});
+
+	LanguageManager.defineLanguage("solidity", {
+		name: "Solidity",
+		mode: "solidity",
+		fileExtensions: ["sol"]
+	});
+});
